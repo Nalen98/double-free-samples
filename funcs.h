38e84143ebd77c128b4a16499ce8aa19538364ad
Nalen98 <52778977+Nalen98@users.noreply.github.com>
2022-11-24 11:56:46 +0300

    Add files via upload

---
+++ funcs.h
@@ -0,0 +1,23 @@
+void double_free();
+
+void no_double_free();
+
+void df_by_pointer();
+
+void df_with_wrappers();
+
+void wrapper(char *ptr);
+
+void conditional_dfree();
+
+void free_null();
+
+void double_delete();
+
+void intrnl_reassignment();
+
+void reassignment(char *ptr);
+
+void good_goto();
+
+void bad_goto();
