58de7ab58ff48d4c7aa107ce7d99d09c7fd62f41
Nalen98 <52778977+Nalen98@users.noreply.github.com>
2022-12-02 17:08:19 +0300

    Fix ugly tab size conflict
    Tabs to spaces convertion
    
    Fix agly tabs

--- funcs.cpp
+++ funcs.cpp
@@ -8,130 +8,130 @@ using namespace std;
 // bad
 void double_free()
 {
-    char *buf1;
-	buf1 = (char *) malloc(SIZE);
-	free(buf1);
-	free(buf1);
+  char *buf1;
+  buf1 = (char *) malloc(SIZE);
+  free(buf1);
+  free(buf1);
 }
 
 // good
 void no_double_free()
 {
-    char *ptr;
-	ptr = (char *) malloc(SIZE);
-	free(ptr);
-	ptr = (char *) malloc(SIZE);
-	free(ptr);
-    cout << "Good!\n";
+  char *ptr;
+  ptr = (char *) malloc(SIZE);
+  free(ptr);
+  ptr = (char *) malloc(SIZE);
+  free(ptr);
+  cout << "Good!\n";
 }
 
 // bad (buf2)
 void df_by_pointer()
 {
-    char *buf1;
-	char *buf2;
-	char *buf3;
+  char *buf1;
+  char *buf2;
+  char *buf3;
 
-	buf1 = (char *) malloc(SIZE);
-	buf2 = (char *) malloc(SIZE);
-	free(buf1);
-	free(buf2);
+  buf1 = (char *) malloc(SIZE);
+  buf2 = (char *) malloc(SIZE);
+  free(buf1);
+  free(buf2);
 
-	buf3 = (char *) malloc(SIZE);
-	free(buf2);
-	free(buf3);
+  buf3 = (char *) malloc(SIZE);
+  free(buf2);
+  free(buf3);
 }
 
 void wrapper(char *ptr)
 {
-    free(ptr);
-    cout << "Wrapper!\n";
+  free(ptr);
+  cout << "Wrapper!\n";
 }
 
 // bad
 void df_with_wrappers()
 {
-    char *buf1;
-	buf1 = (char *) malloc(SIZE);
-	wrapper(buf1);
-	wrapper(buf1);
+  char *buf1;
+  buf1 = (char *) malloc(SIZE);
+  wrapper(buf1);
+  wrapper(buf1);
 }
 
 // bad
 void conditional_dfree()
 {
-    int condition = 1;
-	char *ptr;
-	ptr = (char *) malloc(SIZE);
-	if (condition){
-		free(ptr);
-	}
-	free(ptr);
-	free(ptr);
+  int condition = 1;
+  char *ptr;
+  ptr = (char *) malloc(SIZE);
+  if (condition){
+    free(ptr);
+  }
+  free(ptr);
+  free(ptr);
 }
 
 // good
 void free_null()
 {
-    char *ptr;
-	ptr = (char *) malloc(SIZE);
-	free(ptr);
-	ptr = NULL;
-	free(ptr);
-    cout << "Null ptr isn't a crime yet!\n";
+  char *ptr;
+  ptr = (char *) malloc(SIZE);
+  free(ptr);
+  ptr = NULL;
+  free(ptr);
+  cout << "Null ptr isn't a crime yet!\n";
 }
 
 //bad
 void double_delete()
 {
-    int *x = new int[101];
-    delete [] x;
-    delete [] x;
+  int *x = new int[101];
+  delete [] x;
+  delete [] x;
 }
 
 void reassignment(char *ptr)
 {
-    ptr = (char *) malloc(SIZE);
-    cout << "Reassignment!\n";
+  ptr = (char *) malloc(SIZE);
+  cout << "Reassignment!\n";
 }
 
 // good
 void intrnl_reassignment()
 {
-    char *ptr;
-	ptr = (char *) malloc(SIZE);
-	free(ptr);
-	reassignment(ptr);
-	free(ptr);
+  char *ptr;
+  ptr = (char *) malloc(SIZE);
+  free(ptr);
+  reassignment(ptr);
+  free(ptr);
 }
 
 //bad
 void bad_goto()
 {
-    char *ptr;
-    ptr = (char *) malloc(SIZE);
-    free(ptr);
-    goto free_me;
+  char *ptr;
+  ptr = (char *) malloc(SIZE);
+  free(ptr);
+  goto free_me;
 
 free_me:
-    free(ptr);
-    return;
+  free(ptr);
+  return;
 }
 
 //good
 void good_goto()
 {
-    char *ptr;	
-	bool cond = true;
-    ptr = (char *) malloc(SIZE);  	
-	if (cond){
-		goto no_free;
-	}    
-	free(ptr);
-	return;
+  char *ptr;	
+  bool cond = true;
+  ptr = (char *) malloc(SIZE);  	
+  if (cond){
+    goto no_free;
+  }    
+  free(ptr);
+  return;
 
 no_free:
-    free(ptr);
-	ptr = NULL;
-    return;
+  free(ptr);
+  ptr = NULL;
+  return;
 }
--- main.cpp
+++ main.cpp
@@ -5,51 +5,49 @@ using namespace std;
 
 int main()
 {
-    int choice;
-    void (*func) (void);
+  int choice;
+  void (*func) (void);
 
-    while (1) {
-
-        cout << "Enter your choice: ";
-        cin >> choice;
-
-        switch (choice)
-        {
-            case 1:
-                double_free();
-                break;
-            case 2:
-                no_double_free();
-                break;
-            case 3:
-                func = df_by_pointer;
-                func();
-                break;
-             case 4:
-                df_with_wrappers();
-                break;
-            case 5:
-                conditional_dfree();
-                break;
-            case 6:
-                free_null();
-                break;
-            case 7:
-                double_delete();
-                break;
-            case 8:
-                intrnl_reassignment();
-                break;
-            case 9:
-                bad_goto();
-                break;
-            case 10:
-                good_goto();
-                break;
-            default:
-                cout << "Be more self-confident!\n";
-                break;
-        }
+  while (1) {
+    cout << "Enter your choice: ";
+    cin >> choice;
+    
+    switch (choice) {
+      case 1:
+        double_free();
+        break;
+      case 2:
+        no_double_free();
+        break;
+      case 3:
+        func = df_by_pointer;
+        func();
+        break;
+      case 4:
+        df_with_wrappers();
+        break;
+      case 5:
+        conditional_dfree();
+        break;
+      case 6:
+        free_null();
+        break;
+      case 7:
+        double_delete();
+        break;
+      case 8:
+        intrnl_reassignment();
+        break;
+      case 9:
+        bad_goto();
+        break;
+      case 10:
+        good_goto();
+        break;
+      default:
+        cout << "Be more self-confident!\n";
+        break;
     }
-    return 0;
+  }
+  return 0;
 }
